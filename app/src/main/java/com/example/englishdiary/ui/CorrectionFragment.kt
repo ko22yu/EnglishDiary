ffde665cbc2934b851340203f666b58dab6cf08b
Koyuki Izumi <g1920506@is.ocha.ac.jp>
2023-11-12 13:37:20 +0900

    Material TextField/Buttonに変更しました

--- app/src/main/java/com/example/englishdiary/ui/CorrectionFragment.kt
+++ app/src/main/java/com/example/englishdiary/ui/CorrectionFragment.kt
@@ -45,7 +45,7 @@ class CorrectionFragment : Fragment() {
         }
 
         // EditTextの入力を監視して、文字が入力されたら添削ボタンのenableを更新する
-        binding.englishCompositionInputField.addTextChangedListener(object : TextWatcher {
+        binding.englishCompositionInputEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {
